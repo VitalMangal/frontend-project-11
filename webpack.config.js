2c4646502ab1bd0026ee2df55a892aa6dcad844e
VitalMangal <rvi90@bk.ru>
2024-02-28 14:24:02 +0500

    autosave

--- webpack.config.js
+++ webpack.config.js
@@ -25,14 +25,26 @@ export default {
   ],
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
-      },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
+          { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+          {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+          },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
+
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
